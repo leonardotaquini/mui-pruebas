9f9b3e13d6c75c9c6368a8a28f83fbd75248eac8
leonardo taquini <leonardogtaquini@gmail.com>
2024-05-15 16:12:57 -0300

    formulario funcional

--- src/auth/components/LoginForm.tsx
+++ src/auth/components/LoginForm.tsx
@@ -1,20 +1,57 @@
-import { Button, Grid, TextField, Typography } from '@mui/material'
+import { Alert, Button, Grid, TextField, Typography } from "@mui/material";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { LoginFormI } from "../interfaces/loginForm";
 
 export const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormI>({
+    defaultValues: { user: "", password: "" },
+  });
+  const onSubmit: SubmitHandler<LoginFormI> = (data) => console.log(data);
+
   return (
-        <Grid component={'form'} container spacing={3} justifyContent={'center'} alignItems={'center'}>
-            <Grid item xs={12} >
-                <Typography align='center' variant="h4">SignIn</Typography>
-            </Grid>
-            <Grid item xs={12} >
-                <TextField fullWidth id="outlined-basic" label="User" variant="outlined" />
-            </Grid>
-            <Grid item xs={12} >
-                <TextField fullWidth id="outlined-basic" label="Password" variant="outlined" type="password"/>
-            </Grid>
-            <Grid item xs={12} >
-                <Button fullWidth variant="contained" >Login</Button>
-            </Grid>
-        </Grid>
-  )
-}
+    <Grid
+      component={"form"}
+      container
+      spacing={3}
+      justifyContent={"center"}
+      alignItems={"center"}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Grid item xs={12}>
+        <Typography align="center" variant="h4">
+          SignIn
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          label="User"
+          variant="outlined"
+          {...register("user", { required: true })}
+          error={!!errors.user}
+          helperText={errors.user && "El campo es requerido"}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          label="Password"
+          variant="outlined"
+          type="password"
+          {...register("password", { required: true })}
+          error={!!errors.password}
+          helperText={errors.password && "El campo es requerido"}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button type="submit" fullWidth variant="contained">
+          Login
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
---
+++ src/auth/interfaces/loginForm.ts
@@ -0,0 +1,4 @@
+export interface LoginFormI {
+    user: string;
+    password: string;
+}
